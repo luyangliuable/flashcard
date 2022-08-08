26cb62050d190887f688350eaaef5d34db20d17d
Luyang Liu <luyangl16@gmail.com>
2022-08-08 13:18:39 +1000

    Added basic react-frontend functionality.
    On branch master
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/component/Card.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from "./component/Card";
+import { useState } from "react";
 
 function App() {
+
+  const [cards, updateCards] = useState({
+    current: 1,
+    "cards": [
+      {
+        id: 1,
+        "confidence": 0,
+        "front": "What framework is the frontend?",
+        "back": "react-js",
+      },
+    ],
+  }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={containerStyle.base}>
+      {
+        cards.cards.filter( card => card.id == 1 ).map(filteredCard => {
+          return (
+              <Card front={ filteredCard.front } back={ filteredCard.back }/>
+          );
+        })}
+      <br />
+      <div style={{ display: "flex", "justifyContent": "space-around" }}>
+        <div className="button" style={buttonStyle}>Confident</div>
+        <div className="button" style={buttonStyle}>Expected</div>
+        <div className="button" style={buttonStyle}>Need Practice</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const buttonStyle = {
+  background: "#00BFFF",
+  color: "#FFF",
+  padding: "8px",
+  borderRadius: 4,
+  cursor: "pointer",
+  marginRight: "4px",
+  fontFamily: "Arial"
+};
+
+
+const containerStyle = {
+  base: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "#FFF",
+    position: "absolute",
+    top: 0,
+    height: "100%",
+    left: 0,
+    width: "100%",
+    boxShadow: "2px 2px 2px #888",
+    flexDirection: "column"
+  },
+};
