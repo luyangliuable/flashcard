3b318fb2eb42388cf4d3786e1bb70b550b9d1b1b
Luyang Liu <luyangl16@gmail.com>
2022-08-11 14:37:27 +1000

    Added documentation.
    Changes to be committed:
    	modified:   dockerfile
    	modified:   server/controllers/card_controller.js
    	modified:   server/main.js
    	modified:   server/models/flashcard.js
    	modified:   src/App.js
    	modified:   src/component/Card.js
    	modified:   src/component/style.css

--- server/controllers/card_controller.js
+++ server/controllers/card_controller.js
@@ -1,34 +1,54 @@
 const FlashCard = require('../models/flashcard');
 
+///////////////////////////////////////////////////////////////////////////////
+//                            Middleware function                            //
+///////////////////////////////////////////////////////////////////////////////
 exports.createCard = async (req, res, next) => {
+    /**
+     * Basically with the controller we want to:
+     * - Have function to add/create a card.
+     * - Have function to retrieve all cards (in deck).
+     * @param req - request argument to the middleware function.
+     * @param res - the response argument to the middleware function
+     * @param next - Callback argument to the middleware function
 
-  console.log(req.body);
-  console.log(req.body);
+     * @returns nothing
+     */
 
-  const new_form = new FlashCard({
-    front: req.body.front,
-    back: req.body.back,
-  });
+    console.log(req.body);
 
-  // await new_form.save((err, result) => {
-  new_form.save((err, result) => {
-    if (err) {
-      console.log("error!" + err);
-      return res.status(400).json({ error: err });
-    } else {
-      return res.status(200).json({ ...result._doc, _id: result.id });
-    }
-  });
+    // Create a new flashcard using FlashCard schema
+    const new_form = new FlashCard({
+        front: req.body.front,
+        back: req.body.back,
+    });
+
+    // Upon saving the form catch the error and display the error
+    new_form.save((err, result) => {
+        if (err) {
+            console.log("error!" + err);
+            return res.status(400).json({ error: err });
+        } else {
+            return res.status(200).json({ ...result._doc, _id: result.id });
+        }
+    });
 };
 
-exports.getCards = async (req, res, next) => {
-  const Cards = await FlashCard.find().select('_id front back confidence');
 
-  if (!Cards) {
-    return res.status(400).json({ error: err });
-  } else {
-    console.log(Cards);
-    return res.status(200).json({ output: Cards });
-  }
+exports.getCards = async (req, res, next) => {
+    ///////////////////////////////////////////////////////////////////////////
+    //                      Get the card by using select                     //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // We want the front, back and confidence to be displayed to the user.
+    // We need _id to update the card
+    const Cards = await FlashCard.find().select('_id front back confidence');
+
+    if (!Cards) {
+        return res.status(400).json({ error: err });
+    } else {
+        console.log(Cards);
+        return res.status(200).json({ output: Cards });
+    }
 };
 
--- server/models/flashcard.js
+++ server/models/flashcard.js
@@ -3,21 +3,26 @@ const Schema = mongoose.Schema;
 
 
 const flashCardSchema = new Schema({
-  front: {
-    type: String,
-    default: "",
-    required: false
-  },
-  back: {
-    type: String,
-    default: "",
-    required: false
-  },
-  confidence: {
-    type: Number,
-    default: 0,
-    required: false
-  },
+    /**
+     * Schema for each flashcard document inside mongodb
+     */
+
+    front: {
+        type: String, // Type needs to be a string
+        default: "", // default value
+        required: true // Is required
+    },
+
+    back: {
+        type: String,
+        default: "",
+        required: true
+    },
+    confidence: {
+        type: Number,
+        default: 0,
+        required: false
+    },
 });
 
 module.exports = mongoose.model('FlashCard', flashCardSchema);
